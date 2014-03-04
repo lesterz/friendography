808d4f9735799e113a0f9610258f90532ee76023
Lester Perez <fgraphtech@gmail.com>
2014-03-04 20:25:51 +0100

    .

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Fbmaps::Application.config.secret_token = 'slkfjiu7837yyr65900sjncbx55241290umbk9ij473n4b56j9k2j3s8x9a83765yfhnrje8721209sedmkju34'
+Fbmaps::Application.config.secret_token = 'secret_token'
