42bff98c42ce52ecb9ee4eb8ed184e9d3baa6809
Lester Perez <fgraphtech@gmail.com>
2014-03-20 18:51:04 +0100

    moving beta to top level url, updating FAQ and About US.

--- config/routes.rb
+++ config/routes.rb
@@ -64,7 +64,7 @@ Fbmaps::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'welcome#splash'
+  root :to => 'search#search'
 
   # See how all your routes lay out with "rake routes"
 
